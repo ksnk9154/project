ac95451ea619a753a0554ca0cfb001a4654c7980
ksnk9154 <ksnk9154@gmail.com>
2025-11-06 19:05:10 +0530

    pack

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,12 @@ export default defineConfig({
     },
   },
   server: {
-    port: 3000,
+    host: '0.0.0.0',
+    port: Number(process.env.PORT) || 3000,
     open: true,
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: Number(process.env.PORT) || 3000,
   },
 })
